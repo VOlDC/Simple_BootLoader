e3ab4b35732d71e01aa7f937a24596774743b7a9
jay <jay@DASPCONSULTANT.COM>
2023-12-21 14:56:20 +0200

    Create utilities.asm
    just some utility file

---
+++ src/utilities.asm
@@ -0,0 +1,11 @@
+%define ENDL 0xA, 0xD
+
+; define bytes
+message: db 'Hello World', ENDL, 0
+input_message: db 'jay: ', 0
+
+; reserve bytes
+buffer resb 256 ; create a buffer to restore 256 bytes
+
+_done:
+    ret
